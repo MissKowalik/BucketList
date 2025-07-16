8f79c2f29249de0f9eeb618a2403b64e5ea79c6d
Linda <lindakowalik@gmail.com>
2025-07-16 14:01:25 +0200

    AddDream: added dream input validation and select theme validation

--- dist/pages/AddDream.js
+++ dist/pages/AddDream.js
@@ -1,5 +1,33 @@
 import { displayUserName } from "../utils/storage.js";
 // selecting elements
 const userName = document.getElementById("user-name");
+const addDreamForm = document.querySelector("form");
+const dreamInput = document.getElementById("dream");
+const themeSelect = document.getElementById("dream-select");
+const dreamErrMsg = document.getElementById("dream-error-message");
+const themeErrMsg = document.getElementById("theme-error-message");
 displayUserName(userName);
+// dream input validation
+function isDreamValid() {
+    const trimmedDream = dreamInput.value.trim();
+    if (trimmedDream === "") {
+        dreamErrMsg.style = "display: block";
+    }
+    else {
+        dreamErrMsg.style = "display: none";
+    }
+}
+// select dream theme validation
+function isThemeValid() {
+    const selectedTheme = themeSelect.value;
+    if (!selectedTheme) {
+        themeErrMsg.style = "display: block";
+    }
+    else {
+        themeErrMsg.style = "display: none";
+    }
+}
+// live validation 
+dreamInput.addEventListener("input", isDreamValid);
+themeSelect.addEventListener("change", isThemeValid);
 //# sourceMappingURL=AddDream.js.map
--- src/pages/AddDream.ts
+++ src/pages/AddDream.ts
@@ -2,5 +2,36 @@ import { displayUserName } from "../utils/storage.js";
 
 // selecting elements
 const userName = document.getElementById("user-name") as HTMLSpanElement;
+const addDreamForm = document.querySelector("form") as HTMLFormElement;
+const dreamInput = document.getElementById("dream") as HTMLInputElement;
+const themeSelect = document.getElementById("dream-select") as HTMLSelectElement;
+const dreamErrMsg = document.getElementById("dream-error-message") as HTMLParagraphElement;
+const themeErrMsg = document.getElementById("theme-error-message") as HTMLParagraphElement;
 
 displayUserName(userName);
+
+// dream input validation
+function isDreamValid() {
+    const trimmedDream = dreamInput.value.trim();
+
+    if (trimmedDream === "") {
+        dreamErrMsg.style = "display: block";
+    } else {
+        dreamErrMsg.style = "display: none";
+    }
+}
+
+// select dream theme validation
+function isThemeValid() {
+    const selectedTheme = themeSelect.value;
+
+    if (!selectedTheme) {
+        themeErrMsg.style = "display: block";
+    } else {
+        themeErrMsg.style = "display: none"
+    }
+}
+
+// live validation 
+dreamInput.addEventListener("input", isDreamValid);
+themeSelect.addEventListener("change", isThemeValid);
