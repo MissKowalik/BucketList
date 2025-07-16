1b2b42332e1816c024f1be063f7b751f243e11b4
Linda <lindakowalik@gmail.com>
2025-07-16 13:27:37 +0200

    AddDream: display logged-in user

--- dist/pages/AddDream.js
+++ dist/pages/AddDream.js
@@ -1,2 +1,5 @@
-"use strict";
+import { displayUserName } from "../utils/storage.js";
+// selecting elements
+const userName = document.getElementById("user-name");
+displayUserName(userName);
 //# sourceMappingURL=AddDream.js.map
---
+++ src/pages/AddDream.ts
@@ -0,0 +1,6 @@
+import { displayUserName } from "../utils/storage.js";
+
+// selecting elements
+const userName = document.getElementById("user-name") as HTMLSpanElement;
+
+displayUserName(userName);
